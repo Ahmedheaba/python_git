09476b3c4022f499f4c54ff9fc5e8970869e2fcb
ahmedheaba <ahmedheaba1911@gmail.com>
2021-02-10 11:27:59 +0200

    Test Branch

--- test1.js
+++ test1.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 console.log("Hello World");
-console.log("For Bounse");
-=======
 console.log("Hello World")
 console.log("hello karim was here");
->>>>>>> 7866a6dbdbfe26cb17b29ac62fc070e3f72e7a0c
+console.log("For Bounse");
